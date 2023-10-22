3bddae9ee58d05365cf733c6aebce84f10d1a49c
ahmedellaban1 <ahmedbale112001@gmail.com>
2023-10-22 13:02:59 +0300

    add file uploader, edit installed app in settings

--- core/settings.py
+++ core/settings.py
@@ -32,12 +32,18 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # default django apps, modules
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # installed modules, apps
+
+    # my apps
+    'job',
 ]
 
 MIDDLEWARE = [
