477b68b9b3e5ec1e62a2f27970d981e47fd3aafe
ahmedellaban1 <ahmedbale112001@gmail.com>
2023-10-31 16:00:48 +0200

    override Job Apply View

--- job/views.py
+++ job/views.py
@@ -1,3 +1,5 @@
+from django.forms.models import BaseModelForm
+from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Job, JobApply
 from django.shortcuts import get_object_or_404
@@ -39,3 +41,16 @@ class JopApplyView(CreateView):
     success_url = '/'
     fields = ['email', 'linkedIn', 'github', 'cv', 'cover_litter']
     template_name = 'job/apply_job.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["page_title"] = f'Apply Job | {self.kwargs["slug"]}'
+        return context
+
+    def form_valid(self, form):
+        job = get_object_or_404(Job, slug=self.kwargs['slug'], id=self.kwargs['id'])
+        job_apply = form.save(commit=False)
+        job_apply.job = job
+        job_apply.save()
+        return super().form_valid(form)
+    
